c3baeabceca8b4f46f627bc0439d74c25ec22d26
FamNew <thenewcomerfamily@msn.com>
2019-02-09 21:48:29 +0300

    first commit

---
+++ challenge14.js
@@ -0,0 +1,15 @@
+/* 
+ -only use HTML, javascript & CSS
+ -high score leaderboard
+ -matching game - teach/practice vocab... colors, numbers, fruit, animals- english, french, german, spanish, swahili
+    need 
+        title section, game section, bottom section with 'control' buttons
+        array of matches 
+        cards to display & hide matches
+        rest button
+        ?hint?
+        ?levels-easy-hard?
+        score -how should it be scored
+        time
+ https://github.com/zero-to-mastery/coding_challenge-14
+*/
